ee133c2abf95811cacded29fb87760c4fe12e1c2
Zane Daniel <118499857+yawaka21@users.noreply.github.com>
2024-08-02 16:38:59 +0800

    AddClient

--- src/Clients.jsx
+++ src/Clients.jsx
@@ -6,10 +6,38 @@ export default function Client() {
   const [loading, setLoading] = useState(true);
   const [currentItem, setCurrentItem] = useState(null);
   const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+  const [AddModalOpen, SetAddOpenModal] = useState(false);
 
-  const openDeleteModal = () => setDeleteModalOpen(true);
+  const [users, SetUsers] = useState({ Name: "", Residency: "" });
 
-  const closeDeleteModal = () => setDeleteModalOpen(false);
+  async function Addusers(e) {
+    e.preventDefault();
+
+    const response = await fetch(
+      "http://localhost:5169/api/ClientApi/SaveClient",
+
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: 0,
+          clientName: users.Name,
+          address: users.Residency,
+        }),
+      }
+    );
+    OpenAddModal();
+  }
+  //  function CloseModal() {
+  //   SetAddOpenModal(!AddModalOpen);
+  // }
+  function OpenAddModal() {
+    SetAddOpenModal(!AddModalOpen);
+  }
+
+  const openDeleteModal = () => setDeleteModalOpen(!deleteModalOpen);
 
   const GetClients = async () => {
     setLoading(true);
@@ -41,7 +69,7 @@ export default function Client() {
           params: { id: currentItem.id },
         });
         setClients(clients.filter((client) => client.id !== currentItem.id));
-        closeDeleteModal();
+        openDeleteModal();
       } catch (error) {
         console.error("Failed to delete user:", error);
       }
@@ -50,6 +78,9 @@ export default function Client() {
 
   return (
     <>
+      <div>
+        <button onClick={OpenAddModal}>Add</button>
+      </div>
       {loading ? (
         <p>Loading...</p>
       ) : (
@@ -58,6 +89,7 @@ export default function Client() {
             {clients.map((c) => (
               <tr key={c.id}>
                 <td>{c.clientName}</td>
+                <td>{c.address}</td>
                 <td>
                   <button
                     onClick={() => {
@@ -83,7 +115,7 @@ export default function Client() {
               <h5 className="text-lg font-semibold">Delete Confirmation</h5>
               <button
                 type="button"
-                onClick={closeDeleteModal}
+                onClick={openDeleteModal}
                 className="text-gray-500 hover:text-gray-700"
               >
                 <i className="fa-solid fa-xmark"></i>
@@ -92,7 +124,7 @@ export default function Client() {
             <p className="mt-4">Are you sure you want to delete this user?</p>
             <div className="flex justify-end mt-4">
               <button
-                onClick={closeDeleteModal}
+                onClick={openDeleteModal}
                 className="text-gray-500 hover:text-gray-700 mr-4"
               >
                 Cancel
@@ -102,6 +134,46 @@ export default function Client() {
                 className="text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-4 py-2"
               >
                 Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {AddModalOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white p-6 rounded-lg max-w-lg w-full mx-4 md:mx-0">
+            <div className="flex justify-between items-center">
+              <h5 className="text-lg font-semibold">Add Madafaking users</h5>
+              <button
+                type="button"
+                className="text-gray-500 hover:text-gray-700"
+              >
+                <i className="fa-solid fa-xmark"></i>
+              </button>
+            </div>
+            <form action="">
+              <input
+                type="text"
+                onChange={(e) => SetUsers({ ...users, Name: e.target.value })}
+                value={users.Name}
+              />
+              <input
+                type="text"
+                onChange={(e) =>
+                  SetUsers({ ...users, Residency: e.target.value })
+                }
+                value={users.Residency}
+              />
+
+              <button onClick={(e) => Addusers(e)}>Add Users</button>
+            </form>
+            <div className="flex justify-end mt-4">
+              <button
+                className="text-gray-500 hover:text-gray-700 mr-4"
+                onClick={OpenAddModal}
+              >
+                Cancel
               </button>
             </div>
           </div>
